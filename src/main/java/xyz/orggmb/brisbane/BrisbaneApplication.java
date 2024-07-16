a228ba2f30cb8201b2536892cdc094650c5be9c1
osangu <osangu.dev@gmail.com>
2024-07-16 13:14:13 +0900

    initial commit

---
+++ src/main/java/xyz/orggmb/brisbane/BrisbaneApplication.java
@@ -0,0 +1,13 @@
+package xyz.orggmb.brisbane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrisbaneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrisbaneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/orggmb/brisbane/BrisbaneApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.orggmb.brisbane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrisbaneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
